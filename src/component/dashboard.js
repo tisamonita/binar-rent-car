99061c53fe24d99190eab644870500185ecc6738
Tisa Monita <tisamonita@mail.ugm.ac.id>
2022-09-16 19:32:24 +0700

    add comment

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -16,6 +16,4 @@ export default function Dashboard(){
         HALO
         </>
     )
-
-
 }
--- src/features/user/star-wars-API.js
+++ src/features/user/star-wars-API.js
@@ -7,6 +7,9 @@ const getAllPlanets = () => {
     return response;
 };
 
+//getAllFilms
+//getFilmById
+
 const starWarsAPI = {
     getAllPlanets,
 }
